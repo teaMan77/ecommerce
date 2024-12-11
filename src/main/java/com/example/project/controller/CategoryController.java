978f5d71b98b9fb2a0900105cf7baf5af28b48c9
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-11 16:55:54 +0530

    Adding ResponsEntity to all the endpoints

--- src/main/java/com/example/project/controller/CategoryController.java
+++ src/main/java/com/example/project/controller/CategoryController.java
@@ -20,20 +20,22 @@ public class CategoryController {
     }
 
     @GetMapping("api/public/categories")
-    public List<Category> getCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> getCategories() {
+        return new ResponseEntity<>(categoryService.getAllCategories(),
+                HttpStatus.OK);
     }
 
     @PostMapping("api/public/categories")
-    public String createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
-        return "Category created";
+        return new ResponseEntity<>("Category created", HttpStatus.OK);
     }
 
     @DeleteMapping("api/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         try {
-            return new ResponseEntity(categoryService.deleteCategory(categoryId), HttpStatus.OK);
+            return new ResponseEntity(categoryService.deleteCategory(categoryId),
+                    HttpStatus.OK);
         }
         catch (ResponseStatusException e) {
             return new ResponseEntity(e.getReason(), HttpStatus.NOT_FOUND);

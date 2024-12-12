8f29b7c33e8b58c789c941dffdf2370238492db7
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-12 11:57:55 +0530

    Reducing boilerplate code with Lombok

--- src/main/java/com/example/project/model/Category.java
+++ src/main/java/com/example/project/model/Category.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "categories")
 public class Category {
 
@@ -13,27 +19,4 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
-    public Category() {
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }

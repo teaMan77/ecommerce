1ea2665c0be0ad0364d923b4a7df1d5cf29baa7f
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-12 14:02:11 +0530

    Using @Valid along with field validation annotations

--- src/main/java/com/example/project/controller/CategoryController.java
+++ src/main/java/com/example/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.example.project.controller;
 
 import com.example.project.model.Category;
 import com.example.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +27,7 @@ public class CategoryController {
     }
 
     @PostMapping("public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category created", HttpStatus.OK);
     }
@@ -43,7 +44,7 @@ public class CategoryController {
     }
 
     @PutMapping("public/categories/{categoryId}")
-    public ResponseEntity<Category> updateCategory(@RequestBody Category category,
+    public ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryId) {
         try {
             return new ResponseEntity<>(categoryService.udpateCategory(category, categoryId),

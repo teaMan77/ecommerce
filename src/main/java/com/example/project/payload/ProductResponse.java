b80686fec243bf589076a96f19e53ba472b79ac4
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-14 11:27:34 +0530

    Setting up DTOs for Product

---
+++ src/main/java/com/example/project/payload/ProductResponse.java
@@ -0,0 +1,15 @@
+package com.example.project.payload;
+
+import com.example.project.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private List<Product> content;
+}

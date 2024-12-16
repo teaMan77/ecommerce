f010d143800fac88588861d17a5fb8e3619d4e91
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-16 11:47:16 +0530

    Implementing relation between users and address

--- src/main/java/com/example/project/model/Address.java
+++ src/main/java/com/example/project/model/Address.java
@@ -1,17 +1,50 @@
 package com.example.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+
+    @NotBlank
+    @Min(value = 2, message = "street name must be at least 2 characters")
+    private String street;
+
+    @NotBlank
+    @Min(value = 2, message = "landmark must be at least 2 characters")
+    private String landmark;
+
+    @NotBlank
+    @Min(value = 2, message = "city name must be at least 2 characters")
+    private String city;
+
+    @NotBlank
+    @Min(value = 2, message = "state name must be at least 2 characters")
+    private String state;
+
+    @NotBlank
+    @Min(value = 2, message = "country name must be at least 2 characters")
+    private String country;
+
+    @NotBlank
+    @Min(value = 6, message = "pincode must be at least 6 characters")
+    private String pincode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
 }
--- src/main/java/com/example/project/model/Role.java
+++ src/main/java/com/example/project/model/Role.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -22,4 +25,7 @@ public class Role {
     public Role(AppRole roleName) {
         this.roleName = roleName;
     }
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 }

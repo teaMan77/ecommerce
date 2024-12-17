7cc276f06f948ce305c1e8a6d50c1ad121cdc8c3
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-17 14:52:00 +0530

    Implementing endpoints to get user details

--- src/main/java/com/example/project/controller/AuthController.java
+++ src/main/java/com/example/project/controller/AuthController.java
@@ -18,12 +18,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.project.model.AppRole;
 import com.example.project.model.Role;
@@ -137,5 +135,26 @@ public class AuthController {
         userRepository.save(user);
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @GetMapping("/username")
+    public String getUsername(Authentication authentication) {
+        if (authentication != null)
+            return authentication.getName();
+        return "";
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserInfoResponse> getUserDetails(Authentication authentication) {
+        if (authentication != null) {
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toList();
+
+            return ResponseEntity.ok(new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
